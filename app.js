807d88882e1cb8c08e334ef8f4606eb3609d3337
Tausi Ahmed <tausifahmed49@gmail.com>
2021-01-07 17:25:57 +0600

    done routes

---
+++ app.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const connectDB = require('./config/db')
+
+// load config
+dotenv.config({ path: './config/config.env' })
+
+connectDB()
+
+const app = express()
+
+// for logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+// handlebars
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', '.hbs')
+
+// static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Routes
+app.use('/', require('./routes/index'))
+
+const PORT = process.env.PORT || 3000
+
+app.listen(
+    PORT,
+    console.log(
+        `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
+)
